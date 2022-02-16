2abd468f0d666335697a4cd216eeb702f5e3c777
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-02-16 10:50:43 +0200

    Added new contracts

---
+++ SMS/Contracts/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using SMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Contracts
+{
+    public interface IProductService
+    {
+        (bool created, string error) Create(CreateViewModel model);
+        (bool isValid, string error) ValidateCreateModel(CreateViewModel model);
+        IEnumerable<ProductListViewModel> GetProducts();
+    }
+}
--- SMS/Contracts/IUserService.cs
+++ SMS/Contracts/IUserService.cs
@@ -10,5 +10,7 @@ namespace SMS.Contracts
     public interface IUserService
     {
         (bool registered, string error) Register(RegisterViewModel model);
+        string Login(LoginViewModel model);
+        string GetUsername(string userId);
     }
 }
