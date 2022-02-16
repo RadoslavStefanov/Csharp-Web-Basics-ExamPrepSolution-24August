7695ad33e7b5b693443afaccfed04fdd46207eba
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-02-16 19:30:30 +0200

    Created CartViewModel

---
+++ SMS/Models/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Models
+{
+    public class CartViewModel
+    {
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+    }
+}
