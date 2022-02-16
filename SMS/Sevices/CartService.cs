67f5ef8475d1d32b1cbdc6ae44e9f3a0b6723f08
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-02-16 19:30:39 +0200

    Added CartService

---
+++ SMS/Sevices/CartService.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Contracts;
+using SMS.Data.Common;
+using SMS.Data.Models;
+using SMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Sevices
+{
+    public class CartService : ICartService
+    {
+        private readonly IRepository repo;
+        public CartService(IRepository _repo)
+        {
+            repo = _repo;
+        }
+        public IEnumerable<CartViewModel> AddProduct(string productId, string userId)
+        {
+            var user = repo.All<User>()
+                .Where(u => u.Id == userId)
+                .Include(u => u.Cart)
+                .ThenInclude(c => c.Products)
+                .FirstOrDefault();
+
+            var product = repo.All<Product>()
+                .FirstOrDefault(p => p.Id == productId);
+
+            user.Cart.Products.Add(product);
+
+            try
+            {
+                repo.SaveChanges();
+            }
+            catch (Exception)
+            { }
+
+            return user
+                .Cart
+                .Products
+                .Select(p => new CartViewModel()
+                {
+                    ProductName = p.Name,
+                    ProductPrice = p.Price.ToString("F2")
+                });
+        }
+
+        public void BuyProducts(string userId)
+        {
+            var user = repo.All<User>()
+                .Where(u => u.Id == userId)
+                .Include(u => u.Cart)
+                .ThenInclude(c => c.Products)
+                .FirstOrDefault();
+
+            user.Cart.Products.Clear();
+
+            repo.SaveChanges();
+        }
+
+        public IEnumerable<CartViewModel> GetProducts(string userId)
+        {
+            var user = repo.All<User>()
+                .Where(u => u.Id == userId)
+                .Include(u => u.Cart)
+                .ThenInclude(c => c.Products)
+                .FirstOrDefault();
+
+            return user
+                .Cart
+                .Products
+                .Select(p => new CartViewModel()
+                {
+                    ProductName = p.Name,
+                    ProductPrice = p.Price.ToString("F2")
+                });
+        }
+
+    }
+}
