d054436598f0e1aa59465d5d296a7fcc34e6793d
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-02-16 19:30:06 +0200

    Created Cart Service

---
+++ SMS/Contracts/ICartService.cs
@@ -0,0 +1,18 @@
+﻿using SMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Contracts
+{
+    public interface ICartService
+    {
+
+        IEnumerable<CartViewModel> AddProduct(string productId, string userId);
+        void BuyProducts(string userId);
+        IEnumerable<CartViewModel> GetProducts(string userId);
+
+    }
+}
