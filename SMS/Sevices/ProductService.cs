913cd89b8d2b810ea51a2512289f7dfb93843ba8
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-02-16 10:51:40 +0200

    Created services for data handling

---
+++ SMS/Sevices/ProductService.cs
@@ -0,0 +1,86 @@
+﻿using SMS.Contracts;
+using SMS.Data.Common;
+using SMS.Data.Models;
+using SMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Sevices
+{
+    public class ProductService : IProductService
+    {
+        private readonly IRepository repo;
+        public ProductService(IRepository _repo)
+        { repo = _repo; }
+
+        public (bool created, string error) Create(CreateViewModel model)
+        {
+            bool created = false; string error = null;
+            var (isValid, validationError) = ValidateCreateModel(model);
+
+            if (!isValid)
+            {
+                return (isValid, validationError);
+            }
+
+            Product product = new Product()
+            {
+                Name = model.Name,
+                Price = (decimal)model.Price
+            };
+
+            try
+            {
+                repo.Add(product);
+                repo.SaveChanges();
+                created = true;
+            }
+            catch (Exception)
+            {
+                error = "Couldn't create the product in DB!";
+            }
+            return (created, error);
+        }
+
+        public IEnumerable<ProductListViewModel> GetProducts()
+        {
+            return repo.All<Product>()
+                .Select(p => new ProductListViewModel()
+                {
+                    ProductName = p.Name,
+                    ProductPrice = $"${p.Price.ToString("F2")}",
+                    ProductId = p.Id
+                })
+                .ToList(); 
+        }
+
+        public (bool isValid, string error) ValidateCreateModel(CreateViewModel model)
+        {
+            bool isValid = true;
+            StringBuilder error = new StringBuilder();
+
+            if (model == null)
+            {
+                return (false, "Create model is required!");
+            }
+
+            if (String.IsNullOrWhiteSpace(model.Name) || model.Name.Length < 4 || model.Name.Length > 20)
+            {
+                isValid = false;
+                error.AppendLine("Product name must be between 4 and 20 characters!");
+            }
+
+            if (model.Price < (decimal)0.05D || model.Price > (decimal)1000D)
+            {
+                isValid = false;
+                error.AppendLine($"Product price must be between 0.05 and 1000 characters! You entered {model.Price}");
+            }
+
+            return (isValid, error.ToString());
+
+        }
+    }
+}
--- SMS/Sevices/UserService.cs
+++ SMS/Sevices/UserService.cs
@@ -1,8 +1,11 @@
 ﻿using SMS.Contracts;
+using SMS.Data.Common;
+using SMS.Data.Models;
 using SMS.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +13,70 @@ namespace SMS.Sevices
 {
     public class UserService : IUserService
     {
+        private readonly IRepository repo;
+
+        public UserService(IRepository _repo)
+        { repo = _repo; }
+
+        public string GetUsername(string userId)
+        {
+            return repo.All<User>()
+                .FirstOrDefault(u => u.Id == userId)?.Username;
+        }
+
+        public string Login(LoginViewModel model)
+        {
+            var user = repo.All<User>()
+                .Where(u => u.Username == model.Username)
+                .Where(u => u.Password == CalculateHash(model.Password))
+                .SingleOrDefault();
+
+            return user?.Id;
+        }
+
         public (bool registered, string error) Register(RegisterViewModel model)
         {
-            var(isValid,error)=ValidateRegisterModel(model);
+            bool registered = false; string error = null; 
+
+            var(isValid,validationError)=ValidateRegisterModel(model);
 
             if (!isValid)
             {
-                return (isValid, error);
+                return (isValid, validationError);
+            }
+
+            Cart cart = new Cart();
+
+            User user = new User()
+            {
+                Email = model.Email,
+                Password = CalculateHash(model.Password),
+                Username = model.Username,
+                Cart = cart,
+                CartId = cart.Id
+            };
+
+            try
+            {
+                repo.Add(user);
+                repo.SaveChanges();
+                registered = true;
+            }
+            catch (Exception)
+            {
+                error = "Couldn't save user in DB!";
+            }
+
+            return (registered, error);
+        }
+
+        private string CalculateHash(string password)
+        {
+            byte[] passworArray = Encoding.UTF8.GetBytes(password);
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return Convert.ToBase64String(sha256.ComputeHash(passworArray));
             }
         }
 
@@ -24,14 +84,14 @@ namespace SMS.Sevices
         {
             bool isValid = true;
 
-            StringBuilder error = null;
+            StringBuilder error = new StringBuilder();
 
             if (model == null)
             {
                 return (false, "Register model is required!");
             }
 
-            if (String.IsNullOrWhiteSpace(model.Username) || model.Username.Length <= 5 || model.Username.Length >= 20)
+            if (String.IsNullOrWhiteSpace(model.Username) || model.Username.Length < 5 || model.Username.Length > 20)
             {
                 isValid = false;
                 error.AppendLine("Username must be between 5 and 20 charecters!");
@@ -43,7 +103,7 @@ namespace SMS.Sevices
                 error.AppendLine("The provided Email is not valid!");
             }
 
-            if (String.IsNullOrWhiteSpace(model.Password) || model.Password.Length <= 6 || model.Password.Length >= 20)
+            if (String.IsNullOrWhiteSpace(model.Password) || model.Password.Length < 6 || model.Password.Length > 20)
             {
                 isValid = false;
                 error.AppendLine("Password must be between 6 and 20 charecters!");
