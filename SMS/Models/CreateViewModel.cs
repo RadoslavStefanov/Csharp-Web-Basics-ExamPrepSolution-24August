a16a50d57075797be923c11f98fccfffc6df2b00
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-02-16 10:51:26 +0200

    Added coresponding ViewModels

---
+++ SMS/Models/CreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Models
+{
+    public class CreateViewModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ SMS/Models/ProductListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Models
+{
+    public class ProductListViewModel
+    {
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string  ProductPrice { get; set; }
+    }
+}
